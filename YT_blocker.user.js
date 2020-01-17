c25fb99ed0106e3775422297496c144a6b887f61
timdeu <timdeu@gmail.com>
2020-01-17 16:52:12 +0100

    YT blocker script 0.11

---
+++ YT_blocker.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         YT blocker
+// @namespace    http://tampermonkey.net/
+// @version      0.11
+// @description  only see subscriptions.
+// @author       Peter J.
+// @match        https://www.youtube.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @run-at       document-start
+// ==/UserScript==
+
+
+// https://simply-how.com/enhance-and-fine-tune-any-web-page-the-complete-user-scripts-guide
+// https://www.tampermonkey.net/documentation.php
+
+let lastRun = 0;
+
+setInterval(function() {
+    let timeDif = new Date().getTime() - lastRun;
+    if (timeDif > 5000) {
+
+        let url = window.location.href;
+        if (url.match(/youtube.com\/$/gi)) {
+            var link = document.URL.replace(/youtube.com\/$/gi, "youtube.com/feed/subscriptions");
+            window.location.href=link;
+            lastRun = new Date().getTime();
+        }
+    }
+}, 70);
+
+function del_element(selector) {
+    let element = document.querySelector(selector);
+    if (element) {
+        element.remove();
+    }
+}
+
+setInterval(function rem_elements() {
+    let logo = document.querySelector("#logo");
+    if (logo) {
+        var newEl = document.createElement('div');
+        newEl.innerHTML = "<a href='https://www.youtube.com/feed/subscriptions' style='font-size: 25px;'>Home</a>";
+        logo.parentNode.replaceChild(newEl, logo);
+    }
+
+    del_element('#sections .ytd-guide-renderer:nth-child(1) .ytd-guide-section-renderer:nth-child(1) paper-item');
+    del_element('#items a[title="Home"]');
+}, 500);
